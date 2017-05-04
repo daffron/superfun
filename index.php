fc2eac1c4abdcd5d01140d88c23522b6d7631ce4
EDA <eda@Mac-Mini-7.local>
2017-05-05 08:27:36 +1200

    added contact form php

--- index.php
+++ index.php
@@ -1,4 +1,86 @@
-<!DOCTYPE html>
+<?php
+if(isset($_GET['email'])) {
+
+
+    $email_to = "trey@superfunparties.co.nz";
+    $email_subject = "Website Enquiry";
+
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+
+
+    // validation expected data exists
+    if(!isset($_GET['name']) ||
+        !isset($_GET['email']) ||
+        !isset($_GET['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+
+
+    $name = $_GET['name']; // required
+    $email_from = $_GET['email']; // required
+    $comments = $_GET['comments']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+  }
+
+
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+
+
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+
+<div class=text-center>
+Thank you for contacting me, Ill be in touch very soon
+</div>
+<?php
+
+}
+?>
+
+
 <html lang="en">
 
 <head>
@@ -247,6 +329,7 @@ I am always innovating and looking for new things to provide at kids parties, fr
         <p><span class="glyphicon glyphicon-envelope"></span> trey@superfunparties.co.nz</p>
       </div>
       <div class="col-sm-7">
+        <form name="contactform" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
         <div class="row">
           <div class="col-sm-6 form-group">
             <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
